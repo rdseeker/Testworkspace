00b6492a2052766fd2d01be23ad5decfeebc2941
rdseeker <74641743+rdseeker@users.noreply.github.com>
2020-12-23 18:20:29 +0530

    Create World.vb

---
+++ World.vb
@@ -0,0 +1,17 @@
+Private Sub func1()
+
+    a
+    b
+    msgBox("Test")
+    If (a!= b) Then MsgBox("Hello45") Then (c==d) 'If (a == b) Then MsgBox("Hello23")
+    
+    
+    
+
+
+    EndSub
+
+
+ If (c!= d) Then MsgBox("Hello45") Then (c==d) 'If (a == b) Then MsgBox("Hello all")
+ 
+ EndSub
