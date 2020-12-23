09aa9a12c362f2a24ffa93cf8d80e3448a961894
rdseeker <74641743+rdseeker@users.noreply.github.com>
2020-12-23 18:23:01 +0530

    Update Hello.vb

--- Hello.vb
+++ Hello.vb
@@ -3,7 +3,7 @@ Private Sub func1()
     a
     b
     msgBox("Test")
-    If (a!= b) Then MsgBox("Hello45") Then (c==d) 'If (a == b) Then MsgBox("Hello23")
+    If (c!= d) Then MsgBox("Hello45") Then (a==b) 'If (a == b) Then MsgBox("Hello23")
 
 
     EndSub
